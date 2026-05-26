1c7f99c84f5603f9ef49239414703b25d81172fc
Hugo Gruson <git@hugogruson.fr>
2026-05-26 12:20:23 +0200

    Remove impossible zarr v2 handling case
    Since by the time we get there, the metadata has been harmonized to v3

--- R/ZarrArraySeed-class.R
+++ R/ZarrArraySeed-class.R
@@ -90,20 +90,17 @@ setMethod("show", "ZarrArraySeed",
 .extract_chunkdim_from_metadata <- function(metadata)
 {
     stopifnot(is.list(metadata), !is.null(names(metadata)))
-    chunkdim <- metadata$chunks  # only in Zarr v2
-    if (is.null(chunkdim)) {
-        chunk_grid <- metadata$chunk_grid  # only in Zarr v3
-        if (is.null(chunk_grid))
-            stop(wmsg("unable to determine the chunk dimensions ",
-                      "for this Zarr dataset"))
-        if (!identical(chunk_grid$name, "regular"))
-            stop(wmsg("only Zarr datasets with a regular chunk ",
-                      "grid are supported at the moment"))
-        chunkdim <- chunk_grid$configuration$chunk_shape
-        if (is.null(chunkdim))
-            stop(wmsg("unable to determine the chunk dimensions ",
-                      "for this Zarr dataset"))
-    }
+    chunk_grid <- metadata$chunk_grid  # only in Zarr v3
+    if (is.null(chunk_grid))
+        stop(wmsg("unable to determine the chunk dimensions ",
+                    "for this Zarr dataset"))
+    if (!identical(chunk_grid$name, "regular"))
+        stop(wmsg("only Zarr datasets with a regular chunk ",
+                    "grid are supported at the moment"))
+    chunkdim <- chunk_grid$configuration$chunk_shape
+    if (is.null(chunkdim))
+        stop(wmsg("unable to determine the chunk dimensions ",
+                    "for this Zarr dataset"))
     if (is.list(chunkdim))
         chunkdim <- unlist(chunkdim, use.names=FALSE)
     if (!is.numeric(chunkdim))
