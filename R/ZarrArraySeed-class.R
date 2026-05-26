8e52f5bc14e9a7046f83c7f01e2765db08fa3239
Hugo Gruson <git@hugogruson.fr>
2026-05-26 12:18:07 +0200

    Remove outdated temporary fix

--- R/ZarrArraySeed-class.R
+++ R/ZarrArraySeed-class.R
@@ -49,12 +49,7 @@ setMethod("chunkdim", "ZarrArraySeed", function(x) x@chunkdim)
 setMethod("extract_array", "ZarrArraySeed",
     function(x, index)
     {
-        ans <- Rarr::read_zarr_array(x@zarr_path, index, x@s3_client)
-        ## Temporary fix.
-        ## See https://github.com/Huber-group-EMBL/Rarr/issues/137
-        if (typeof(ans) != x@type)
-            storage.mode(ans) <- x@type
-        ans
+        Rarr::read_zarr_array(x@zarr_path, index, x@s3_client)
     }
 )
 
