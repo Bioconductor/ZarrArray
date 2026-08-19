99dce4e8a4ff6e49a95d8ce605c1780b5a3b82e8
Hugo Gruson <git@hugogruson.fr>
2026-08-19 18:43:35 +0200

    Add support for bfloat
    Follow up from https://github.com/Huber-group-EMBL/Rarr/pull/179

--- R/utils.R
+++ R/utils.R
@@ -33,7 +33,7 @@ zarrtype2Rtype <- function(base_type)
     stopifnot(isSingleString(base_type))
     switch(base_type, bool="logical",
                       int=, uint="integer",
-                      float="double",
+                      bfloat=, float="double",
                       #complex="complex",
                       string=, unicode="character",
                       stop(wmsg("unrecognized Zarr base type: ", base_type)))
